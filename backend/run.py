5ea6913599756ac0495704a601774f674ec5e9e2
mavibilisimcom <mb08m@hotmail.com>
2026-09-14 12:10:21 +0300

    Wire user data module

--- backend/run.py
+++ backend/run.py
@@ -5,26 +5,14 @@ from provider_dispatch import dispatch_notification
 from providers_admin import router as provider_router
 from venue_profiles import router as venue_profile_router
 from marketplace import router as marketplace_router
+from user_data import router as user_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
 app.include_router(provider_router)
 app.include_router(venue_profile_router)
 app.include_router(marketplace_router)
+app.include_router(user_router)
 
-scheduler.add_job(
-    lambda: process_due_occasions(dispatch_notification),
-    "cron",
-    hour=10,
-    minute=0,
-    id="occasion_notifications",
-    replace_existing=True,
-)
-
-scheduler.add_job(
-    lambda: process_scheduled_campaigns(dispatch_notification),
-    "interval",
-    minutes=5,
-    id="scheduled_notification_campaigns",
-    replace_existing=True,
-)
+scheduler.add_job(lambda: process_due_occasions(dispatch_notification),'cron',hour=10,minute=0,id='occasion_notifications',replace_existing=True)
+scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),'interval',minutes=5,id='scheduled_notification_campaigns',replace_existing=True)
