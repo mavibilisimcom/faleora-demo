ba06a8a4fb7659ee91839bb020757dd32eff2554
mavibilisimcom <mb08m@hotmail.com>
2026-09-18 20:08:58 +0300

    Wire POS and KDS backend

--- backend/run.py
+++ backend/run.py
@@ -8,6 +8,7 @@ from marketplace import router as marketplace_router
 from user_data import router as user_router
 from screen_network import router as screen_router
 from venue_os import router as venue_os_router
+from pos_core import router as pos_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -17,6 +18,7 @@ app.include_router(marketplace_router)
 app.include_router(user_router)
 app.include_router(screen_router)
 app.include_router(venue_os_router)
+app.include_router(pos_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
