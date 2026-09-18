d4ffafc7c23ce9645ebe000685a595aca46352cf
mavibilisimcom <mb08m@hotmail.com>
2026-09-18 17:37:56 +0300

    Wire FALEORA Screen backend

--- backend/run.py
+++ backend/run.py
@@ -6,6 +6,7 @@ from providers_admin import router as provider_router
 from venue_profiles import router as venue_profile_router
 from marketplace import router as marketplace_router
 from user_data import router as user_router
+from screen_network import router as screen_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -13,6 +14,7 @@ app.include_router(provider_router)
 app.include_router(venue_profile_router)
 app.include_router(marketplace_router)
 app.include_router(user_router)
+app.include_router(screen_router)
 
-scheduler.add_job(lambda: process_due_occasions(dispatch_notification),'cron',hour=10,minute=0,id='occasion_notifications',replace_existing=True)
-scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),'interval',minutes=5,id='scheduled_notification_campaigns',replace_existing=True)
+scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
+scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
