096cec54ddcc31ce0953bb7b148e837744a7aa01
mavibilisimcom <mb08m@hotmail.com>
2026-09-18 20:53:04 +0300

    Wire RBAC menu engine and payments

--- backend/run.py
+++ backend/run.py
@@ -11,6 +11,9 @@ from venue_os import router as venue_os_router
 from pos_core import router as pos_router
 from operations import router as operations_router
 from commercial_core import router as commercial_core_router
+from access_control import router as access_router
+from menu_engine import router as menu_engine_router
+from payments_core import router as payments_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -23,6 +26,9 @@ app.include_router(venue_os_router)
 app.include_router(pos_router)
 app.include_router(operations_router)
 app.include_router(commercial_core_router)
+app.include_router(access_router)
+app.include_router(menu_engine_router)
+app.include_router(payments_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
