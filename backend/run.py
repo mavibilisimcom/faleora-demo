b67d29fd837b46fa4638dc6205596a5ad44ab108
mavibilisimcom <mb08m@hotmail.com>
2026-09-19 21:12:45 +0300

    Wire real session authentication API

--- backend/run.py
+++ backend/run.py
@@ -19,6 +19,7 @@ from reservations import router as reservations_router
 from purchasing import router as purchasing_router
 from workforce import router as workforce_router
 from health import router as health_router
+from auth import router as auth_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -39,6 +40,7 @@ app.include_router(reservations_router)
 app.include_router(purchasing_router)
 app.include_router(workforce_router)
 app.include_router(health_router)
+app.include_router(auth_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
