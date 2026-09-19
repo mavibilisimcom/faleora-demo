ecfb87f12b8e594ec9fe64edeaf4d336f9e3133f
mavibilisimcom <mb08m@hotmail.com>
2026-09-19 13:49:59 +0300

    Wire health readiness endpoints

--- backend/run.py
+++ backend/run.py
@@ -18,6 +18,7 @@ from checkout_orchestrator import router as checkout_router
 from reservations import router as reservations_router
 from purchasing import router as purchasing_router
 from workforce import router as workforce_router
+from health import router as health_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -37,6 +38,7 @@ app.include_router(checkout_router)
 app.include_router(reservations_router)
 app.include_router(purchasing_router)
 app.include_router(workforce_router)
+app.include_router(health_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
