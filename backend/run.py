a03d05bccb8394508d291a4f0954c67cf5691baa
mavibilisimcom <mb08m@hotmail.com>
2026-09-20 10:42:49 +0300

    Wire stock audit and cash reconciliation pilot core

--- backend/run.py
+++ backend/run.py
@@ -22,6 +22,8 @@ from health import router as health_router
 from auth import router as auth_router
 from offline_sync import router as offline_sync_router
 from sales_engine import router as sales_router
+from pilot_core import router as pilot_router
+from cash_reconciliation import router as cash_reconciliation_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -45,6 +47,8 @@ app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(offline_sync_router)
 app.include_router(sales_router)
+app.include_router(pilot_router)
+app.include_router(cash_reconciliation_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
