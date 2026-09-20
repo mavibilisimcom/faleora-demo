13d2e82986709f107f04d0509486d2218102e3ed
mavibilisimcom <mb08m@hotmail.com>
2026-09-20 10:14:59 +0300

    Wire unified POS sales engine

--- backend/run.py
+++ backend/run.py
@@ -21,6 +21,7 @@ from workforce import router as workforce_router
 from health import router as health_router
 from auth import router as auth_router
 from offline_sync import router as offline_sync_router
+from sales_engine import router as sales_router
 
 app.include_router(occasion_router)
 app.include_router(campaign_router)
@@ -43,6 +44,7 @@ app.include_router(workforce_router)
 app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(offline_sync_router)
+app.include_router(sales_router)
 
 scheduler.add_job(lambda: process_due_occasions(dispatch_notification),"cron",hour=10,minute=0,id="occasion_notifications",replace_existing=True)
 scheduler.add_job(lambda: process_scheduled_campaigns(dispatch_notification),"interval",minutes=5,id="scheduled_notification_campaigns",replace_existing=True)
